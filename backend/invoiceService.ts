2e8abefdc88ad21af1bc026bc84285c65cd0b8d3
danleks <danilevich.lks@gmail.com>
2022-09-04 17:42:58 +0200

    created invoice service

---
+++ backend/invoiceService.ts
@@ -0,0 +1,21 @@
+import { invoices } from './data/invoices';
+
+const getInvoices = () => {
+  return invoices;
+};
+const addInvoice = () => {
+  //
+};
+const deleteInvoice = () => {
+  //
+};
+const updateInvoice = () => {
+  //
+};
+
+export default {
+  getInvoices,
+  addInvoice,
+  deleteInvoice,
+  updateInvoice,
+};
