23dcf5ea3e771b896d2b8695397cd811d9c71426
danleks <danilevich.lks@gmail.com>
2022-09-05 18:55:54 +0200

    create getPreviewForInvoiceEntry

--- backend/src/services/invoiceService.ts
+++ backend/src/services/invoiceService.ts
@@ -3,6 +3,15 @@ import { invoices } from '../../data/invoices';
 const getInvoices = () => {
   return invoices;
 };
+const getPreviewDataForInvoiceEntry = () => {
+  return invoices.map(({ id, paymentDue, clientName, total, status }) => ({
+    id,
+    paymentDue,
+    clientName,
+    total,
+    status,
+  }));
+};
 const addInvoice = () => {
   //
 };
@@ -18,4 +27,5 @@ export default {
   addInvoice,
   deleteInvoice,
   updateInvoice,
+  getPreviewDataForInvoiceEntry,
 };
